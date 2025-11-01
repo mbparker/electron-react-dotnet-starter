f76b6b22b98976e8b6f3d75ffa9453116f9b0822
mb.parker <mb.parker@gmail.com>
2025-11-01 00:53:59 -0400

    App side changes (along with libsqlite3orm) to remove Desktop directory access.

--- app-api/ElectronAppApiTestHarness/Program.cs
+++ app-api/ElectronAppApiTestHarness/Program.cs
@@ -3,16 +3,20 @@
 
 using Autofac;
 using ElectronAppApiTestHarness;
+using LibElectronAppApi.Shared;
+using LibElectronAppApi.Shared.Abstract;
 using LibElectronAppDemo.Abstract;
 using LibElectronAppDemo.Database;
 using LibElectronAppDemo.Database.Models;
 using LibSqlite3Orm;
-using LibSqlite3Orm.Abstract;
 using LibSqlite3Orm.Abstract.Orm;
 
 try
 {
     using var container = ContainerRegistration.RegisterDependencies();
+    var fileOps = container.Resolve<IFileOperations>();
+    ConsoleLogger.Filename = Path.Combine(fileOps.GetLocalAppDataPathForCurrentPlatform(), SharedConstants.AppName,
+        "Logs", $"TestHarnessConsoleLog-{DateTime.Today.Year}-{DateTime.Today.Month}-{DateTime.Today.Day}.log");
     var ormTracer = container.Resolve<IOrmGenerativeLogicTracer>();
 
     ormTracer.SqlStatementExecuting += (sender, args) =>
@@ -49,5 +53,5 @@ try
 }
 finally
 {
-    ConsoleLogger.Dispose();
+    ConsoleLogger.Filename = null;
 }
